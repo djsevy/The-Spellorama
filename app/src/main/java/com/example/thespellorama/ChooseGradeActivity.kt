5a17a0f5831c5463af51e8497f1e6b90629c60ee
deoelvenmage <deoelvenmage@gmail.com>
2022-05-31 22:12:48 -0600

    Added a ChooseGrade page; kotlin and xml file

---
+++ app/src/main/java/com/example/thespellorama/ChooseGradeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.thespellorama
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseGradeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_grade)
+    }
+}
