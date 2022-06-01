ee07475949defc30d5023a46429cfed229f58e05
Grant Holley <grant.holley98@gmail.com>
2022-06-01 12:45:06 -0600

    Game mode activity

---
+++ app/src/main/java/com/example/thespellorama/GameModeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.thespellorama
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameModeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_mode)
+    }
+}
