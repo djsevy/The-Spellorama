372e94eec15ca15e193b9cececb3729ad1276faa
Joe Hawkins <hawkinsjwh@gmail.com>
2022-06-07 12:30:37 -0600

    fixed the manifest

--- app/src/main/java/com/example/thespellorama/MainActivity.kt
+++ app/src/main/java/com/example/thespellorama/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.thespellorama
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Adding navigation to the Difficulty selection screen...
+
+        val navigateToDifficulty = findViewById<Button>(R.id.k6Button)
+
+        navigateToDifficulty.setOnClickListener{
+
+            val difficultyScreen = Intent(this,setDifficulty::class.java)
+            startActivity(difficultyScreen)
+        }
+
     }}
 
