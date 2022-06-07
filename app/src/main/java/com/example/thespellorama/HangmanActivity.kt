d554ce58c46ff9ff36899cf153074160c36493fa
Joe Hawkins <hawkinsjwh@gmail.com>
2022-06-07 13:08:09 -0600

    Fixed the manifest, added navigation to all pages, styled, tidied up

--- app/src/main/java/com/example/thespellorama/HangmanActivity.kt
+++ app/src/main/java/com/example/thespellorama/HangmanActivity.kt
@@ -1,11 +1,25 @@
 package com.example.thespellorama
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HangmanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hangman)
+
+        // Adding navigation to the Home screen...
+
+        val navigateToHome = findViewById<Button>(R.id.hangmanHomeButton)
+
+        navigateToHome.setOnClickListener{
+
+            val homeScreen = Intent(this,MainActivity::class.java)
+            startActivity(homeScreen)
+        }
+
+
     }
 }
