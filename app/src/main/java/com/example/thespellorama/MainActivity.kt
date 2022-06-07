d664048641a0511efa9733353bd0b4170ab3b022
Joe Hawkins <hawkinsjwh@gmail.com>
2022-06-07 12:08:00 -0600

    added to main

--- app/src/main/java/com/example/thespellorama/MainActivity.kt
+++ app/src/main/java/com/example/thespellorama/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.thespellorama
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // Button that leads to the set difficulty activity...
+
+
+
+
+
 
     // Create new name for the btnBack
     homeButton.setOnClickListener(new OnClickListener(){
