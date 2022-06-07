5e6ff426ae32fa4ce5d60eec35bfe834d005f165
Joe Hawkins <hawkinsjwh@gmail.com>
2022-06-07 12:09:06 -0600

    fixed the main activity

--- app/src/main/java/com/example/thespellorama/MainActivity.kt
+++ app/src/main/java/com/example/thespellorama/MainActivity.kt
@@ -7,16 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    // Create new name for the btnBack
-    homeButton.setOnClickListener(new OnClickListener(){
-
-        private void onClick(){
-            Intent intent = new Intent(currentActivity.this, Main.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(intent);
-        }
-    });
-}
+    }}
 
