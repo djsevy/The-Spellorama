f9ed3143990ea6a317d61083013ca50ca550df64
djsevy <dallinsevy@gmail.com>
2022-06-15 08:56:17 -0600

    Added class words, fun wordSelect, and wordBreakdown.

--- app/src/main/java/com/example/thespellorama/HangmanActivity.kt
+++ app/src/main/java/com/example/thespellorama/HangmanActivity.kt
@@ -1,9 +1,11 @@
 package com.example.thespellorama
+import java.util.Random
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import java.util.*
 
 class HangmanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,12 +16,48 @@ class HangmanActivity : AppCompatActivity() {
 
         val navigateToHome = findViewById<Button>(R.id.hangmanHomeButton)
 
-        navigateToHome.setOnClickListener{
+        navigateToHome.setOnClickListener {
 
-            val homeScreen = Intent(this,MainActivity::class.java)
+            val homeScreen = Intent(this, MainActivity::class.java)
             startActivity(homeScreen)
         }
 
 
     }
+        class Words {
+            val theWords = mutableListOf("David", "Grant", "Clark", "Joseph", "Dallin")
+            val previousWords = mutableListOf("")
+            val correctWords = mutableListOf("")
+
+
+
+            fun main() {
+
+
+            }
+
+            fun wordSelect(): String {
+                val random = Random()
+                val randIndex = random.nextInt(theWords.size)
+                return theWords[randIndex]
+            }
+
+            fun wordBreakdown(): MutableList<String> {
+                val letters = mutableListOf("")
+                for (letter in theWords)
+                    letters.add(letter)
+                return letters
+            }
+
+            fun userInput(){
+
+            }
+
+            //TODO: add fun to compare users guess against randomly chosen word.
+        }
+
+
+
+
+
 }
