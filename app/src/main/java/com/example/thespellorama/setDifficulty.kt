e4673ddbd5b57386a7005acb34f3a124e89a1acd
clarkscoberly <cob18002@byui.edu>
2022-07-05 12:57:11 -0600

    Updated TODO within setDifficulty.ky

--- app/src/main/java/com/example/thespellorama/setDifficulty.kt
+++ app/src/main/java/com/example/thespellorama/setDifficulty.kt
@@ -14,12 +14,24 @@ class setDifficulty : AppCompatActivity() {
 
         val navigateToGameMode = findViewById<Button>(R.id.kindergartenButton)
 
-        navigateToGameMode.setOnClickListener{
+                navigateToGameMode.setOnClickListener{
 
             val gameModeScreen = Intent(this,GameModeActivity::class.java)
             startActivity(gameModeScreen)
         }
 
+        // TODO - Give the necessary list of words to pass to the (Game)Activity
+        // The following is a basic outline of the TODO item.
+        /*
+        switch statement
+        switch 1 (Kidergarten) {
+            get kidergarten file and either get a word and return it to the next activity
+            or get a package or pointer to that kidergarten file.
+        }
+        switch 2 (Grade 1) {
+            ETC.
+        }
+         */
         // Difficulty Selection Idea:
         // The difficulty will be based on 7 different files corresponding to the grade selected.
         // Kindergarten will have 5 easy words in it's file, First grade will add 3 more "harder" words to it's file and so forth
